01a367238c86674243528af1d721b105f32c30ff
Olga Nazarova <headcr@bk.ru>
2020-10-11 23:57:35 +0300

    Add Scene2

---
+++ Assets/Scripts/GameObjects/Sphere.cs
@@ -0,0 +1,18 @@
+﻿using Assets.Scripts.Interfaces;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour, ITick
+{
+    void Awake()
+    {
+        //Toolbox.GetOrCreate<UpdateManager>().Add(this);
+        UpdateManager.Add(this);
+    }
+
+    public void Tick()
+    {
+        Debug.Log($"I'm {gameObject.name}!");
+    }
+}
