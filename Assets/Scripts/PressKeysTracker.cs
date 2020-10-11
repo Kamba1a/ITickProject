f58beca7a41350006bfb440f4b4edf737c4bc82c
Olga Nazarova <headcr@bk.ru>
2020-10-11 23:58:06 +0300

    Add switching between scenes

---
+++ Assets/Scripts/PressKeysTracker.cs
@@ -0,0 +1,28 @@
+﻿using Assets.Scripts.Interfaces;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PressKeysTracker : MonoBehaviour, ITick
+{
+    void Awake()
+    {
+        UpdateManager.Add(this);
+    }
+    public void Tick()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (SceneManager.GetActiveScene().name == "Scene1")
+            {
+                SceneManager.LoadScene("Scene2");
+            }
+            else
+            {
+                SceneManager.LoadScene("Scene1");
+            }
+            Toolbox.ClearTools();
+        }
+    }
+}
