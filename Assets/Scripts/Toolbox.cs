1146094f8ff599a7ead74d5de1f4295a1bd0c8e4
Olga Nazarova <headcr@bk.ru>
2020-10-11 23:54:47 +0300

    Add class Toolbox

---
+++ Assets/Scripts/Toolbox.cs
@@ -0,0 +1,37 @@
+﻿using Assets.Scripts.Interfaces;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.TerrainAPI;
+using UnityEngine;
+
+public class Toolbox : Singleton<Toolbox>
+{
+    Dictionary<Type, object> tools = new Dictionary<Type, object>();
+
+    //public static void Add(object obj)
+    //{
+    //    Instance.tools.Add(obj.GetType(), obj);
+    //    Debug.Log(obj.GetType());
+    //}
+
+    //public static T Get<T>()
+    //{
+    //    Instance.tools.TryGetValue(typeof(T), out object obj);
+    //    return (T)obj;
+    //}
+
+    public static T GetOrCreate<T>()
+    {
+        if (!Instance.tools.ContainsKey(typeof(T)))
+        {
+            Instance.tools.Add(typeof(T), new GameObject("UpdateManager").AddComponent<UpdateManager>());
+        }
+        return (T)Instance.tools[typeof(T)];
+    }
+
+    public static void ClearTools()
+    {
+        Instance.tools.Clear();
+    }
+}
