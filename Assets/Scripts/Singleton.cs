5e527c799448e3e0964133a9abf9ca2d51d92847
Olga Nazarova <headcr@bk.ru>
2020-10-11 23:54:32 +0300

    Add class Singleton

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton <T> : MonoBehaviour where T: MonoBehaviour
+{
+    static T instance;
+    static object locker = new object();
+
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                lock (locker)
+                {
+                    if (instance == null)
+                    {
+                        instance = FindObjectOfType<T>();
+                        if (instance == null)
+                        {
+                            instance = new GameObject(typeof(T).Name).AddComponent<T>();
+                        }
+                    }
+                    DontDestroyOnLoad(instance);
+                }
+            }
+            return instance;
+        }
+    }
+
+    public static T GetThis()
+    {
+        if (instance == null)
+        {
+            lock (locker)
+            {
+                if (instance == null)
+                {
+                    instance = FindObjectOfType<T>();
+                    if (instance == null)
+                    {
+                        instance = new GameObject(typeof(T).Name).AddComponent<T>();
+                    }
+                }
+            }
+        }
+        return instance;
+    }
+}
