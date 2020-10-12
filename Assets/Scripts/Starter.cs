2803b617c4d05d51801b548f5e40e0bc69e88498
Kamba1a <headcr@bk.ru>
2020-10-12 16:28:35 +0300

    Add in scenes GameObjects 'Starter'

---
+++ Assets/Scripts/Starter.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Starter : MonoBehaviour
+{
+    private void Awake()
+    {
+        _ = PressKeysTracker.Instance;  //PressKeysTracker initialize
+    }
+}
