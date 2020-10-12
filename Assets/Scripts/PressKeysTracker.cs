50c83fdb18207de7ce57947cf5c2587c3d9b0f47
Kamba1a <headcr@bk.ru>
2020-10-12 16:27:34 +0300

    Update PressKeysTracker.cs
    Change PressKeysTracker class on Singleton

--- Assets/Scripts/PressKeysTracker.cs
+++ Assets/Scripts/PressKeysTracker.cs
@@ -4,25 +4,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PressKeysTracker : MonoBehaviour, ITick
+public class PressKeysTracker : Singleton<PressKeysTracker>, ITick
 {
     void Awake()
     {
         UpdateManager.Add(this);
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        SceneManager.sceneUnloaded += OnSceneUnloaded;
     }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode) => UpdateManager.Add(this);
+    private void OnSceneUnloaded(Scene current) => Toolbox.ClearTools();
+
     public void Tick()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (SceneManager.GetActiveScene().name == "Scene1")
-            {
-                SceneManager.LoadScene("Scene2");
-            }
-            else
-            {
-                SceneManager.LoadScene("Scene1");
-            }
-            Toolbox.ClearTools();
+            if (SceneManager.GetActiveScene().name == "Scene1") SceneManager.LoadScene("Scene2");
+            else SceneManager.LoadScene("Scene1");
         }
     }
 }
