20afdf1afd750ab555349a8917748aaebc5b94a8
Kamba1a <headcr@bk.ru>
2020-10-12 13:51:48 +0300

    Update Singleton.cs
    Delete GetThis method (duplicate functionality)

--- Assets/Scripts/Singleton.cs
+++ Assets/Scripts/Singleton.cs
@@ -29,23 +29,4 @@ public class Singleton <T> : MonoBehaviour where T: MonoBehaviour
             return instance;
         }
     }
-
-    public static T GetThis()
-    {
-        if (instance == null)
-        {
-            lock (locker)
-            {
-                if (instance == null)
-                {
-                    instance = FindObjectOfType<T>();
-                    if (instance == null)
-                    {
-                        instance = new GameObject(typeof(T).Name).AddComponent<T>();
-                    }
-                }
-            }
-        }
-        return instance;
-    }
 }
